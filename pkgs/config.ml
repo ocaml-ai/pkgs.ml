3bbf6e506447cda43b6fcf9f133fd80441b142b1
Leandro Ostera <leandro@abstractmachines.dev>
2024-04-25 15:27:17 +0100

    nicer config errors

--- pkgs/config.ml
+++ pkgs/config.ml
@@ -1,6 +1,16 @@
+let env ?default var =
+  let exception Missing_configuration of string in
+  match (Sys.getenv_opt var, default) with
+  | None, None ->
+      raise
+        (Missing_configuration
+           (Format.sprintf "Missing environment variable %s" var))
+  | None, Some x -> x
+  | Some x, _ -> x
+
 let typesense_config =
   Typesense.
     {
-      api_key = Sys.getenv "TYPESENSE_API_KEY";
-      url = Sys.getenv "TYPESENSE_API_URL";
+      api_key = env ~default:"dummy" "TYPESENSE_API_KEY";
+      url = env ~default:"dummy" "TYPESENSE_API_URL";
     }
