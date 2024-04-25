6dcc380196b53f5fd6a8534df5324905beefc390
sabine <6594573+sabine@users.noreply.github.com>
2024-04-25 21:41:55 +0200

    navigate from search results to package info page

--- pkgs/url.ml
+++ pkgs/url.ml
@@ -1,5 +1,8 @@
 let package_download_url (package_info : Package_registry.add_package_request) =
-  Package_registry.(
-    Printf.sprintf {|https://pkgs.ml/p/%s/%s/%s/%s/%s|} package_info.source
-      package_info.org package_info.repo package_info.ref
-      package_info.package_name)
+  Printf.sprintf {|https://pkgs.ml/p/%s/%s/%s/%s/%s|} package_info.source
+    package_info.org package_info.repo package_info.ref
+    package_info.package_name
+
+let package_info_url (package : Package_search_index.document) =
+  Printf.sprintf {|/i/%s/%s/%s/%s/%s|} package.source package.org
+    package.repo package.ref package.pkg
