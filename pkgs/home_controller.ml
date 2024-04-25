1989ac6ef8df5b785bccd649422a78182a1d1ac2
Leandro Ostera <leandro@abstractmachines.dev>
2024-04-25 20:31:12 +0100

    make room for package lists

--- pkgs/home_controller.ml
+++ pkgs/home_controller.ml
@@ -6,6 +6,22 @@ end)
 
 open Trail
 
+(*
+
+TODO(leostera):
+1. get all packages again
+2. query clickhouse for stats
+3. show stats on homepage
+
+*)
+
 let get conn =
-  let html = Template_home.make ~packages:[] () |> Html_of_jsx.render in
+  (* let (packages, _, _, _) = Package_search_index.search ~q:"" () |> Result.get_ok in *)
+  let total_downloads = 0 in
+  let total_packages = 0 in
+  let latest_packages = [] in
+  let html =
+    Template_home.make ~latest_packages ~total_downloads ~total_packages ()
+    |> Html_of_jsx.render
+  in
   conn |> Conn.send_response `OK {%b|"<!doctype html>"::string,html::string|}
--- pkgs/i18n.ml
+++ pkgs/i18n.ml
@@ -2,4 +2,5 @@ let title = "pkgs.ml"
 let tagline = "An OCaml Package Registry"
 
 let description =
-  "Instantly access your OCaml and Reason packages and install them."
+  "Instantly access your OCaml and Reason packages hosted on Github and \
+   install them with zero fuss."
